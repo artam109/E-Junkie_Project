3a190e3578e9dcb97665e0d638d4aed9e92d9d03
Artam <erdemmmgurel@gmail.com>
2024-03-20 14:21:32 +0300

    US_307_NavigateToHomepage_Test_Case > Passed

---
+++ src/US_307_NavigateToHomepage.java
@@ -0,0 +1,22 @@
+import Utility.BaseDriver;
+import Utility.E_Junkie_Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class US_307_NavigateToHomepage extends BaseDriver {
+    @Test
+    public void NavigateToHomepage() {
+
+        // Click Navigation Bar logo, check for if it's navigates to the homepage
+        eje.homePageBtn.click();
+        wait.until(ExpectedConditions.urlToBe("https://shopdemo.e-junkie.com/"));
+        Assert.assertEquals(driver.getCurrentUrl(), "https://shopdemo.e-junkie.com/");
+
+        WebElement closeBox = driver.findElement(By.cssSelector("div[class=\"columns is-mobile\"] a[href=\"/\"]"));
+        closeBox.click();
+
+    }
+}
