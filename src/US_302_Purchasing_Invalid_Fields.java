ebbefcf948203d03898aa78781e013a6fc181552
Artam <erdemmmgurel@gmail.com>
2024-03-20 13:45:47 +0300

    US_302_Purchasing_Invalid_Fields_Test_Case > Passed

---
+++ src/US_302_Purchasing_Invalid_Fields.java
@@ -0,0 +1,28 @@
+import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class US_302_Purchasing_Invalid_Fields extends BaseDriver {
+    @Test
+    public void PurchasingInvalidFields() {
+
+        // add e-book to the cart
+        eje.addToCartBtn.click();
+
+        // click Pay Debit button
+        WebElement cartFrame = driver.findElement(By.xpath("//iframe[@class=\"EJIframeV3 EJOverlayV3\"]"));
+        driver.switchTo().frame(cartFrame);
+        eje.payDebitBtn.click();
+
+        // Click pay button and assert warning box text
+        eje.payBtn.click();
+        String[] invalid_fields = eje.invalidPaymentDetailsTxt.getText().split("\n");
+        for (String txt : invalid_fields) {
+            Assert.assertTrue(eje.invalidPaymentDetailsTxt.getText().contains(txt));
+        }
+
+    }
+
+}
